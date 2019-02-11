c9e270ccd19ec7849f53bc0b60c6cc0f020d206d
Harald Sitter <sitter@kde.org>
2019-02-11 08:55:08 +0100

    install appstream

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 apt-key adv --keyserver keyserver.ubuntu.com --recv E6D4736255751E5D
 echo 'deb http://archive.neon.kde.org/user bionic main' > /etc/apt/sources.list.d/neon.list
 apt update
-apt install apt-config-icons apt-config-icons-hidpi apt-config-icons-large apt-config-icons-large-hidpi -y
+apt install apt-config-icons apt-config-icons-hidpi apt-config-icons-large apt-config-icons-large-hidpi appstream -y
 
 while :
 do
