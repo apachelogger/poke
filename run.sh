979d7ab37454997306e85da37cfea45b25c439a8
Harald Sitter <sitter@kde.org>
2019-02-11 08:44:01 +0100

    import

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-key adv --keyserver keyserver.ubuntu.com --recv E6D4736255751E5D
+echo 'deb http://archive.neon.kde.org/user bionic main' > /etc/apt/sources.list.d/neon.list
+apt update
+apt install apt-config-icons apt-config-icons-hidpi apt-config-icons-large apt-config-icons-large-hidpi -y
+
+while :
+do
+
+apt update
+sleep 4
+
+done
---
+++ spin.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+@conts = []
+
+def clean
+  @conts.each do |c|
+    system "docker rm -f -v #{c}"
+  end
+exit
+end
+
+trap ("SIGINT") {
+clean
+}
+
+128.times do |i|
+puts i
+name = "cont-#{i}"
+system "docker rm -f -v #{name}"
+system "docker create -v `pwd`:/workspace --name #{name} ubuntu:16.04 /workspace/run.sh"
+system "docker start #{name}"
+@conts << name
+end
+
+sleep 10000000000000000
+
+clean
